629398d7978ca40d9bff9a7d04326fd0501c3913
André Formento <andreformento.sc@gmail.com>
2018-08-31 22:10:56 -0300

    factorial exercise

--- hangman-game/src/exercises/core.clj
+++ hangman-game/src/exercises/core.clj
@@ -1,9 +1,9 @@
 (ns exercises.core
   (:gen-class))
 
-;; fib(0) = 0
-;; fib(1) = 1
-;; fib(n) = fib(n-1) + fib(n-2), se n >= 2
+; fib(0) = 0
+; fib(1) = 1
+; fib(n) = fib(n-1) + fib(n-2), se n >= 2
 
 (defn fibonacci [n]
   (if (= n 0)
@@ -11,6 +11,27 @@
     (if (= n 1)
       1
       (+ (fibonacci(- n 1)) (fibonacci(- n 2)))
+    )
+  )
+)
+
+; (exercises/fibonacci-rec 3)
+(defn fibonacci-rec [x]
+  (loop [a 1 b 1 index 2]
+    (if
+      (= index x) b
+      (recur b (+ a b) (inc index))
+    )
+  )
+)
+
+; (exercises/factorial 5)
+; when n = 5, then result = 15
+(defn factorial [n]
+  (loop [accumulator 0 index 1]
+    (if
+      (> index n) accumulator
+      (recur (+ accumulator index) (inc index))
     )
   )
 )
